64bbfcfb5dc8c02d7a13171a462e62c90fdcdea9
Łukasz Urbaś <lukasz.urbas@britenet.com.pl>
2018-01-22 13:46:14 +0100

    .

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -27,8 +27,57 @@ mainApp.controller('EmployeesCtrl', ['$scope','EmployeesService',
       $scope.getEmployees = function () {
           EmployeesService.getEmployees()
             .then(function success(response) {
+                $scope.itemsPerPage = 10;
+                $scope.currentPage = 0;
                 $scope.employees = response.data;
-            });
+
+                $scope.range = function() {
+                    var rangeSize = 4;
+                    var ps = [];
+                    var start;
+
+                    start = $scope.currentPage;
+                    if ( start > $scope.pageCount()-rangeSize ) {
+                      start = $scope.pageCount()-rangeSize+1;
+                    }
+
+                    for (var i=start; i<start+rangeSize; i++) {
+                      if(i>=0)
+                              ps.push(i);
+                    }
+                    return ps;
+                  };
+
+                  $scope.prevPage = function() {
+                    if ($scope.currentPage > 0) {
+                      $scope.currentPage--;
+                    }
+                  };
+
+                  $scope.DisablePrevPage = function() {
+                    return $scope.currentPage === 0 ? "disabled" : "";
+                  };
+
+                  $scope.pageCount = function() {
+                    return Math.ceil($scope.employees.length/$scope.itemsPerPage)-1;
+                  };
+
+                  $scope.nextPage = function() {
+                    if ($scope.currentPage < $scope.pageCount()) {
+                      $scope.currentPage++;
+                    }
+                  };
+
+                  $scope.DisableNextPage = function() {
+                    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+                  };
+
+                  $scope.setPage = function(n) {
+                    $scope.currentPage = n;
+                  };
+
+                }
+            )
       }
 
       $scope.addEmployee = function () {
@@ -78,3 +127,12 @@ mainApp.service('EmployeesService', [ '$http', function($http) {
       }
 
 } ]);
+
+angular.module('mainApp').filter('pagination', function()
+{
+  return function(input, start) {
+    if (!input || !input.length) { return; }
+    start = parseInt(start, 10);
+    return input.slice(start);
+  };
+});
